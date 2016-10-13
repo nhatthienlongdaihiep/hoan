39d82aa7a8cf403c55ce9575fd165242545d52a9
user <user@Users-Computer-4.local>
2016-10-13 09:58:15 +0700

    vd b3

--- bai3.php
+++ bai3.php
@@ -2,7 +2,7 @@
 $a = array("1"=>"b", "2"=>"y", "3"=>"h", "4"=>"j", "5"=>"e", "6"=>"t", "7"=>"u", "8"=>"q");
 //krsort($a);
 
-$new = array_slice($a, 0 , 6);
+$new = array_slice($a, 1 , 5);
 
 echo '<pre>';
 print_r( $new );
